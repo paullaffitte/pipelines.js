7a6a4a9c28adf338cc9664a7cd34b52ae3eea507
Paul Laffitte <paul.laffitte@epitech.eu>
2020-10-17 17:05:48 +0700

    add documentation

--- src/ExecutionNode.ts
+++ src/ExecutionNode.ts
@@ -1,23 +1,42 @@
+/** The result of an execution. */
 type Result<T> = T | Promise<T>;
 
+/** A function that execute something given parameters and returns a {@link Result}. */
 export type Executor<T, U> = (config: T) => Result<U>;
 
+/** A function that execute a list of {@link ExecutionNode} and returns a result. */
 export type ExecutionHandler<T, U> = (nodes: ExecutionNode<T, U>[]) => Result<U>;
 
+/** A function that do something given an {@link ExecutionNode} */
 export type Hook = (node: ExecutionNode<any, any>) => any;
 
+/**
+ * A node in an execution tree.
+ */
 export class ExecutionNode<T, U> {
 	private executor: Executor<T, U>;
 	private _config: T;
 	private hooks: Hook[];
 	private _output?: U;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param {Executor<T, U>} executor The executor to be executed on the given configuration.
+	 * @param {T} config The config to be passed to the executor on execution.
+	 * @param {Hook[]} [hooks] Callbacks to be executed after each {@link Executor} execution.
+	 */
 	constructor(executor: Executor<T, U>, config: T, hooks: Hook[] = []) {
 		this.executor = executor;
 		this._config = config;
 		this.hooks = hooks;
 	}
 
+	/**
+	 * Execute this node and his sub-tree if it has one.
+	 *
+	 * @returns The output of the execution.
+	 */
 	public async execute(): Promise<U> {
 		this._output = await this.executor(this.config);
 		this.hooks.map(hook => hook(this));
@@ -25,10 +44,20 @@ export class ExecutionNode<T, U> {
 		return this._output;
 	}
 
+	/**
+	 * Get the output (readonly)
+	 *
+	 * @returns
+	 */
 	get output(): U | undefined {
 		return this._output;
 	}
 
+	/**
+	 * Get the config (readonly)
+	 *
+	 * @returns
+	 */
 	get config(): T {
 		return this._config;
 	}
--- src/Pipelines.ts
+++ src/Pipelines.ts
@@ -1,14 +1,31 @@
 import { ExecutionHandler, ExecutionNode, Executor, Hook } from './ExecutionNode';
 
+/**
+ * The main execution class, its purpose is to build {@link ExecutionNode}s.
+ * It features some default {@link ExecutionHandler}s.
+ */
 export default class Pipelines<T, U> {
 	private executor: Executor<T, U>;
 	private hooks?: Hook[];
 
+	/**
+	 * Constructor.
+	 *
+	 * @param {Executor<T, U>} executor The executor used at leaves level in the execution tree.
+	 * @param {Hook[]} [hooks] Callbacks to be executed after each {@link ExecutionNode} execution (not only the leaves).
+	 */
 	constructor(executor: Executor<T, U>, hooks?: Hook[]) {
 		this.executor = executor;
 		this.hooks = hooks;
 	}
 
+	/**
+	 * Adds an {@link ExecutionNode} to the tree, the execution node SHOULD NOT be a leaf.
+	 *
+	 * @param {ExecutionHandler<T, U>} executionHandler The execution handler handling childrens execution.
+	 * @param {ExecutionNode<T, U>[]} nodes Childrens to execute.
+	 * @returns The newly created {@link ExecutionNode}.
+	 */
 	public node(
 		executionHandler: ExecutionHandler<T, U>,
 		nodes: ExecutionNode<T, U>[]
@@ -16,23 +33,52 @@ export default class Pipelines<T, U> {
 		return new ExecutionNode(executionHandler, nodes, this.hooks);
 	}
 
+	/**
+	 * Adds an {@link ExecutionNode} to the tree, the execution node MUST be a leaf.
+	 *
+	 * @param {ExecutionNode<T, U>} config The config to be passed to the node when it will be executed.
+	 * @returns The newly created {@link ExecutionNode}.
+	 */
 	public exec(config: T): ExecutionNode<T, U> {
 		return new ExecutionNode(this.executor, config, this.hooks);
 	}
 
+	/**
+	 * Create a new {@link Pipelines} with a new default executor but the same hooks.
+	 *
+	 * @param {Executor<V, W>} executor The new default executor to be used.
+	 * @returns The newly created {@link Pipelines}.
+	 */
 	public with<V, W>(executor: Executor<V, W>): Pipelines<V, W> {
 		return new Pipelines(executor, this.hooks);
 	}
 
+	/**
+	 * Execute a list of {@link ExecutionNode} sequentially.
+	 *
+	 * @param {ExecutionNode<any, any>[]} nodes The list of nodes to be executed.
+	 * @returns The newly created {@link ExecutionNode}.
+	 */
 	public sequence(nodes: ExecutionNode<any, any>[]): ExecutionNode<ExecutionNode<T, U>[], U> {
 		return this.node(executionHandlers.sequence, nodes);
 	}
 
+	/**
+	 * Execute a list of {@link ExecutionNode} in parallel.
+	 *
+	 * @param {ExecutionNode<any, any>[]} nodes The list of nodes to be executed.
+	 * @returns The newly created {@link ExecutionNode}.
+	 */
 	public parallel(nodes: ExecutionNode<any, any>[]): ExecutionNode<ExecutionNode<T, U>[], U> {
 		return this.node(executionHandlers.parallel, nodes);
 	}
 }
 
+/**
+ * Default {@link ExecutionHandler}s used in {@link Pipelines}.
+ *
+ * @param {ExecutionNode<any, any>[]} nodes The list of nodes to be executed.
+ */
 export const executionHandlers: Record<string, ExecutionHandler<any, any>> = {
 	sequence: async nodes => {
 		const outputs = [];
